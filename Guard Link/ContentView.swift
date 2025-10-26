4c1840827d0d4ac50d5204c7ec9093008b1f9b7a
Rakshith J <rakshith.jk2@gmail.com>
2025-10-25 20:53:19 -0500

    Initial Commit

---
+++ Guard Link/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guard Link
+//
+//  Created by Rakshith Jayakarthikeyan on 10/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Guard Link/Guard_LinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guard_LinkApp.swift
+//  Guard Link
+//
+//  Created by Rakshith Jayakarthikeyan on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Guard_LinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
